dc155abbbc256ed18b41f338619460e88ee36b7c
Frank Rodriguez <lokenator73@gmail.com>
2018-01-27 18:46:12 -0500

    Initial Commit Finished Main Screen & 2D UI

---
+++ TwoDShapeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ShapeCalculatorWPF
+{
+    /// <summary>
+    /// Interaction logic for TwoDShapeWindow.xaml
+    /// </summary>
+    public partial class TwoDShapeWindow : Window
+    {
+        public TwoDShapeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
