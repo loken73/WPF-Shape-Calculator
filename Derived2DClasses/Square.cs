33394ecd6bc9f543f527f39c6487fcb7c28704fa
Frank Rodriguez <lokenator73@gmail.com>
2018-01-28 11:33:14 -0500

    Added Square to 2D shapes

---
+++ Derived2DClasses/Square.cs
@@ -0,0 +1,34 @@
+﻿using ShapeCalculatorWPF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatorWPF.Derived2DClasses
+{
+    public class Square : TwoDimensionalShape, INotCircular
+    {
+        private int _sideLength;
+
+        public override double Area {
+            get
+            {
+                return _sideLength * _sideLength;
+            }
+        }
+
+        public int Perimeter
+        {
+            get
+            {
+                return _sideLength * 4;
+            }
+        }
+
+        public Square (int sideLength)
+        {
+            this._sideLength = sideLength;
+        }
+    }
+}
