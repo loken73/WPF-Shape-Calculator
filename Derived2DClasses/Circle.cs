d413d36bdb5db0c331ba43b9a34ec32b495231b8
Frank Rodriguez <lokenator73@gmail.com>
2018-01-27 23:41:06 -0500

    Added Abstract Class for 2D and 3D shapes and Interface for Circle and non circle shapes

---
+++ Derived2DClasses/Circle.cs
@@ -0,0 +1,13 @@
+﻿using ShapeCalculatorWPF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatorWPF.Derived2DClasses
+{
+    public class Circle : TwoDimensionalShape, ICircular
+    {
+    }
+}
---
+++ Interfaces/INotCircular.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatorWPF.Interfaces
+{
+    public interface INotCircular
+    {
+        int Perimeter { get; }
+    }
+}
