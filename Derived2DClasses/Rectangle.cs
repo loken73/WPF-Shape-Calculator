6e114bdb31c48c564f40cf675c3db53133561bba
Frank Rodriguez <lokenator73@gmail.com>
2018-01-28 13:34:09 -0500

    All 2D shape classes set up

---
+++ Derived2DClasses/Rectangle.cs
@@ -0,0 +1,37 @@
+﻿using ShapeCalculatorWPF.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeCalculatorWPF.Derived2DClasses
+{
+    class Rectangle : TwoDimensionalShape, INotCircular
+    {
+        private int _length;
+
+        private int _width;
+
+        public override double Area
+        {
+            get
+            {
+                return _length * _width;
+            }
+        }
+
+        public int Perimeter
+        {
+            get
+            {
+                return _length * 2 + _width * 2;
+            }
+        }
+
+        public Square(int sideLength)
+        {
+            this._sideLength = sideLength;
+        }
+    }
+}
